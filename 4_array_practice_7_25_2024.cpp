9a606446cae9e0590c95c837815387c58b862109
TaFhiM12 <tafhim0000001@gmail.com>
2024-07-25 16:08:15 +0600

    3rd Commit

--- 4_array_practice_7_25_2024.cpp
+++ 4_array_practice_7_25_2024.cpp
@@ -5,9 +5,33 @@ using namespace std ;
 #define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
 #define fraction(n) cout << fixed << setprecision(n);
 
+int a[1000];
+int n ;
+
+void displayArray(int *a , int n ){
+    for(int i=0 ; i<n ; i++){
+        cout << a[i] <<  " ";
+    }
+    cout << endl ;
+}
+
+void insertElement(int *a , int x , int index){
+    for(int i=n ; i>index ; i--){
+        a[i] = a[i-1];
+    }
+
+    a[index] = x ;
+    n++;
+}
+
 int32_t main()
 {
     optimize();
-    
+    cin >> n ;
+    for(int i=0 ; i<n ; i++) cin >> a[i];
+    int x , index; 
+    cin >> x >> index ;
+    insertElement(a , x ,index);
+    displayArray(a,n);
     return 0;
 }
